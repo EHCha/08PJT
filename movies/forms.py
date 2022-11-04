1906cdcffc1682a4636793b1258c05c383c4f384
EHCha <chaeh0926@gmail.com>
2022-11-04 09:35:47 +0900

    movies_partial

--- movies/forms.py
+++ movies/forms.py
@@ -1,3 +1,17 @@
 from django import forms
-
+from .models import Genre, Movie
 # Create your forms here.
+
+
+class GenreForm(forms.ModelForm):
+
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
+
+class MovieForm(forms.ModelForm):
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
--- movies/views.py
+++ movies/views.py
@@ -1,17 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.http import require_safe
-
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods, require_POST, require_safe
+from .models import Genre, Movie
+from .forms import GenreForm, MovieForm
 
 # Create your views here.
 @require_safe
 def index(request):
-    pass
-
+    movies = Movie.object.all()
+    context = {
+        'movies': movies,
+    }
+    return render(request, 'movies/index.html', context)
 
 @require_safe
 def detail(request, movie_pk):
-    pass
-
+    movie = Movie.object.get(movie_pk=movie_pk)
+    context = {
+        'movie': movie,
+    }
+    return render(request, 'movies/detail.html', context)
 
 @require_safe
 def recommended(request):
